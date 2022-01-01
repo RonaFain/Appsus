f73002aa83f3c057930a799f46846dd656c770c7
SaharGar <ssahar456@gmail.com>
2022-01-01 22:57:24 +0200

    fixed video fullscreen

--- js/apps/keep/cmps/VideoNote.jsx
+++ js/apps/keep/cmps/VideoNote.jsx
@@ -5,7 +5,7 @@ export function VideoNote({note}){
     return (
         <section className="video-note note">
             <h2>{note.info.title}</h2>
-            <iframe width="100%" heigth="200" src={note.info.url}/>
+            <iframe width="100%" heigth="200" src={note.info.url} allowFullScreen/>
         </section>
     )
 }
