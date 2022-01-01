1fdafe8907bb24bf3ad45d068da2f269c9574a81
SaharGar <ssahar456@gmail.com>
2022-01-02 01:44:43 +0200

    fixing more typos

--- js/apps/keep/cmps/NoteFilter.jsx
+++ js/apps/keep/cmps/NoteFilter.jsx
@@ -1,4 +1,4 @@
-
+import { utilService } from "../../../services/util.service.js"
 
 export function NoteFilter({ notesTypes, onSetTypeFilter, currType }) {
 
@@ -7,7 +7,7 @@ export function NoteFilter({ notesTypes, onSetTypeFilter, currType }) {
             <h2>Filter notes by type:</h2>
             <div className="filter-btns">
                 {notesTypes.map(type => {
-                    return <button title={type} className={type === currType ? 'active' : ''}
+                    return <button title={utilService.capitalFirstLetter(type)} className={type === currType ? 'active' : ''}
                         key={type} onClick={() => onSetTypeFilter(type)} >
                         <img src={`assets/imgs/filter-by-${type}.png`} /></button>
                 })}
